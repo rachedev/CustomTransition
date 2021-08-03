d7177e736b857f33fda22248818b8f281abd6279
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-02 21:32:27 -0700

    Initial Commit

---
+++ CustomTransition/CustomTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTransitionApp.swift
+//  CustomTransition
+//
+//  Created by RachelleLee on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
